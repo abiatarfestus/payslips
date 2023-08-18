7c2208d2c2eca854fcebfd46b42fc080fe36124f
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2023-08-18 21:13:05 +0200

    Update version number

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from load_gui import MainWindow
 
 # ====================================LOAD APPLICATION=======================================#
 app = QApplication(sys.argv)
-app.setApplicationName("Payslips Distributor v1.0")
+app.setApplicationName("Payslips Distributor v1.1")
 app.setStyle("Fusion")
 home = MainWindow()
 widget = QtWidgets.QStackedWidget()
